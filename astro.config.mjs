4818353c5b95224938c6bf44e5c3ab98ecb22a74
24watchin <24watchin.news@gmail.com>
2026-03-15 22:14:18 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 const siteUrl = process.env.SITE_URL || 'https://example.com';
@@ -10,4 +11,5 @@ const siteUrl = process.env.SITE_URL || 'https://example.com';
 export default defineConfig({
   site: siteUrl,
   integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
